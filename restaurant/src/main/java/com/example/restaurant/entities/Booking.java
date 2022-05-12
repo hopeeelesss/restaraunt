c923a93e1937ba8480837d9316f015269c64e15c
hopeeelesss <89143187+hopeeelesss@users.noreply.github.com>
2022-05-13 02:04:19 +0300

    Update Booking.java

--- restaurant/src/main/java/com/example/restaurant/entities/Booking.java
+++ restaurant/src/main/java/com/example/restaurant/entities/Booking.java
@@ -1,17 +1,32 @@
 package com.example.restaurant.entities;
 
 import lombok.Data;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.sql.Date;
 
 @Data
+@Entity
+@Table
 public class Booking {
-    int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Integer id;
     Date date;
     String name;
     String surname;
     String telephone;
+    public Booking() {}
+
+    public Booking(Date date, String name, String surname, String telephone) {
+        this.date = date;
+        this.name = name;
+        this.surname = surname;
+        this.telephone = telephone;
+    }
 }
